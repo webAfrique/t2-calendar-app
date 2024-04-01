270704b0573792a0a0483ce639a405ad341b375b
Wen Yan <selinyan2016@gmail.com>
2024-04-01 12:22:03 +0300

    create component Footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./component/Header";
 import "./App.css";
 import Register from "./component/Register";
+import Footer from "./component/Footer";
 
 function App() {
   return (
     <div>
       <Header />
       <Register />
+      <Footer />
     </div>
   );
 }
