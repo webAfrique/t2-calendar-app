cd3f4c8a20f02e5e05e044342d3e97b3315fb0b1
Wen Yan <selinyan2016@gmail.com>
2024-04-04 11:20:55 +0300

    not creating component Hero, but create Home route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import Register from "./routes/Register";
 import Root from "./routes/Root";
 import Login from "./routes/Login";
+import Home from "./routes/Home";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Root />}>
+          <Route index element={<Home />} />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
         </Route>
---
+++ client/src/routes/Home.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { Box, Button, Typography } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const defaultTheme = createTheme();
+
+const Home = () => {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Box
+        sx={{
+          margin: 0,
+          height: "calc(100vh - 68.5px - 76.2px)",
+          backgroundImage: "url(../../public/hero_image.jpg)",
+          backgroundRepeat: "no-repeat",
+          backgroundColor: (t) =>
+            t.palette.mode === "light"
+              ? t.palette.grey[50]
+              : t.palette.grey[900],
+          backgroundSize: "100% 100%",
+          backgroundPosition: "center",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "0 50px",
+        }}>
+        <Typography
+          component="h1"
+          variant="h2"
+          gutterBottom
+          sx={{
+            fontFamily: "Inter",
+            fontSize: "64px",
+            fontWeight: "bold",
+            color: "#00A8CD",
+            textAlign: "center",
+          }}>
+          Design your own
+          <br /> Christmas calendar
+        </Typography>
+        <Typography
+          component="h2"
+          variant="h4"
+          gutterBottom
+          sx={{
+            fontFamily: "Inter",
+            fontSize: "24px",
+            fontWeight: "bold",
+            color: "#00A8CD",
+            textAlign: "center",
+            marginBottom: "20px",
+          }}>
+          Use our platform to customise your digital advent calendar
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          sx={{
+            backgroundColor: "#476C92",
+            color: "#FFFFFF",
+            textAlign: "center",
+          }}>
+          Create now
+        </Button>
+      </Box>
+    </ThemeProvider>
+  );
+};
+
+export default Home;
