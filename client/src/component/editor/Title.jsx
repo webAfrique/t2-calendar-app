9f42e66cda1f9b177ff9628298ebef2a7b224186
Inna Kozhina <inna.kozhina22@gmail.com>
2024-04-09 16:15:12 +0300

    added title component to editor page component, replaced title comp to editor folder

--- client/src/component/editor/Title.jsx
+++ client/src/component/editor/Title.jsx
@@ -1,19 +1,10 @@
 import * as React from "react";
-import PropTypes from "prop-types";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
 import Divider from "@mui/material/Divider";
-import Drawer from "@mui/material/Drawer";
-import IconButton from "@mui/material/IconButton";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 import Collapse from "@mui/material/Collapse";
 import ExpandLess from "@mui/icons-material/ExpandLess";
@@ -24,24 +15,12 @@ import FormatAlignLeftIcon from "@mui/icons-material/FormatAlignLeft";
 import FormatAlignCenterIcon from "@mui/icons-material/FormatAlignCenter";
 import FormatAlignRightIcon from "@mui/icons-material/FormatAlignRight";
 import FormatAlignJustifyIcon from "@mui/icons-material/FormatAlignJustify";
-import Paper from "@mui/material/Paper";
-import { styled } from "@mui/material/styles";
 import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import { MuiColorInput } from "mui-color-input";
 import FormatBoldIcon from "@mui/icons-material/FormatBold";
 import FormatItalicIcon from "@mui/icons-material/FormatItalic";
 import FormatUnderlinedIcon from "@mui/icons-material/FormatUnderlined";
-
-const drawerWidth = 260;
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
 
 const fontFamilies = [
   "Roboto",
@@ -53,10 +32,7 @@ const fontFamilies = [
   "Courier New",
 ];
 
-function ResponsiveDrawer(props) {
-  const { window } = props;
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-  const [isClosing, setIsClosing] = React.useState(false);
+function Title() {
   const [open, setOpen] = React.useState(false);
   const [alignment, setAlignment] = React.useState("");
   const [fontFamily, setFontFamily] = React.useState("Roboto");
@@ -64,30 +40,11 @@ function ResponsiveDrawer(props) {
   const [selectedSize, setSelectedSize] = React.useState(16);
   const [fontStyle, setFontStyle] = React.useState([]);
 
-  const handleDrawerClose = () => {
-    setIsClosing(true);
-    setMobileOpen(false);
-  };
-
-  const handleDrawerTransitionEnd = () => {
-    setIsClosing(false);
-  };
-
-  const handleDrawerToggle = () => {
-    if (!isClosing) {
-      setMobileOpen(!mobileOpen);
-    }
-  };
-
   const handleClick = () => {
     setOpen(!open);
   };
 
   const handleAlignment = (event, newAlignment) => {
-    setAlignment(newAlignment);
-  };
-
-  const handleChange = (event, newAlignment) => {
     setAlignment(newAlignment);
   };
 
@@ -124,11 +81,11 @@ function ResponsiveDrawer(props) {
 
   const control = {
     value: alignment,
-    onChange: handleChange,
+    onChange: handleAlignment,
     exclusive: true,
   };
 
-  const drawer = (
+  const titleMenu = (
     <div>
       <Toolbar />
       <Divider />
@@ -156,7 +113,8 @@ function ResponsiveDrawer(props) {
               <ToggleButtonGroup
                 size="small"
                 {...control}
-                aria-label="Small sizes">
+                aria-label="Small sizes"
+              >
                 {children}
               </ToggleButtonGroup>
             </ListItemButton>
@@ -166,7 +124,8 @@ function ResponsiveDrawer(props) {
                 value={fontFamily}
                 size="small"
                 onChange={handleFontFamilyChange}
-                sx={{ padding: 0 }}>
+                sx={{ padding: 0 }}
+              >
                 {fontFamilies.map((font) => (
                   <MenuItem key={font} value={font}>
                     {font}
@@ -191,7 +150,8 @@ function ResponsiveDrawer(props) {
                 value={selectedSize}
                 size="small"
                 onChange={handleSizeChange}
-                sx={{ minWidth: "80px", padding: 0 }}>
+                sx={{ minWidth: "80px", padding: 0 }}
+              >
                 <MenuItem value={12}>12</MenuItem>
                 <MenuItem value={14}>14</MenuItem>
                 <MenuItem value={16}>16</MenuItem>
@@ -210,7 +170,8 @@ function ResponsiveDrawer(props) {
                 size="small"
                 value={fontStyle}
                 onChange={handleFontStyleChange}
-                aria-label="text formatting">
+                aria-label="text formatting"
+              >
                 <ToggleButton value="bold" aria-label="bold">
                   <FormatBoldIcon />
                 </ToggleButton>
@@ -224,60 +185,11 @@ function ResponsiveDrawer(props) {
             </ListItemButton>
           </List>
         </Collapse>
-
-        {/* {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))} */}
-        {/* </List>
-      <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))} */}
       </List>
-      {/* Additional list items can be added as needed */}
     </div>
   );
 
-  // Remove this const when copying and pasting into your project.
-  const container =
-    window !== undefined ? () => window().document.body : undefined;
-
-  return (
-    <Box sx={{ display: "flex" }}>
-      <Box
-        component="nav"
-        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-        aria-label="mailbox folders">
-        <Drawer
-          variant="permanent"
-          sx={{
-            display: { xs: "none", sm: "block" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
-          open>
-          {drawer}
-        </Drawer>
-      </Box>
-    </Box>
-  );
+  return <div>{titleMenu}</div>;
 }
 
-export default ResponsiveDrawer;
+export default Title;
