b159beb2e1647b810df4a66a9e022994e5348066
Mohsin Md <s2301255@BCMX073F6DHsAir.home>
2024-04-13 17:20:22 +0300

    create header and remove footer for editor page and connect logo to admin page

---
+++ client/src/component/editor/EditorHeader.jsx
@@ -0,0 +1,127 @@
+import { Link } from "react-router-dom";
+import { auth } from "../../../../server/firebase"
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+import SaveIcon from "@mui/icons-material/Save";
+import LogoutIcon from "@mui/icons-material/Logout";
+import Logo from "../../assets/WIME-logo.png"
+import PreviewIcon from "@mui/icons-material/Preview";
+import { Container } from "@mui/material";
+
+
+
+function EditorHeader() {
+ 
+
+  return (
+    <AppBar
+      position="static"
+      sx={{ backgroundColor: "#fff", boxShadow: "none" }}
+    >
+      <Container maxWidth="xl">
+      <Toolbar disableGutters>
+        <Link to="/user">
+          <Avatar
+            src={Logo}
+            alt="Logo"
+            sx={{
+              mr: 1,
+              width: 60,
+              height: 60,
+              backgroundColor: "transparent",
+              borderRadius: "50%",
+            }}
+          />
+        </Link>
+
+        <Typography
+          variant="h6"
+          noWrap
+          component="a"
+          href="/"
+          sx={{
+            mr: 2,
+            display: { xs: "none", md: "flex" },
+            fontFamily: "monospace",
+            fontWeight: 700,
+            letterSpacing: ".3rem",
+            color: "#00a8cd",
+            textDecoration: "none",
+          }}
+        >
+          WIME
+        </Typography>
+
+        <Box sx={{ flexGrow: 1 }} />
+        <Box sx={{ p: 0, display: { md: "flex" }, mx: 2}}>
+        <Tooltip title="Preview">
+          <IconButton 
+          sx={{
+            backgroundColor: "#476C92",
+            color: "white",
+            textTransform: "capitalize",
+            "&:hover": {
+              backgroundColor: "white",
+              color: "#476C92",
+              borderColor: "#476C92",
+              boxShadow: "none",
+              border: "1px solid",
+            },
+          }}>
+            <PreviewIcon />
+          </IconButton>
+        </Tooltip>
+        </Box>
+        <Box sx={{ p: 0, display: { md: "flex" }, mx:4}}>
+        <Tooltip title="Save">
+          <IconButton 
+          sx={{
+              backgroundColor: "#476C92",
+              color: "white",
+              textTransform: "capitalize",
+              "&:hover": {
+                backgroundColor: "white",
+                color: "#476C92",
+                borderColor: "#476C92",
+                boxShadow: "none",
+                border: "1px solid",
+              },
+            }}>
+            <SaveIcon />
+          </IconButton>
+        </Tooltip>
+        </Box>
+        <Box sx={{ p: 0, display: { md: "flex" }, mx: 2 }}>
+        <Link to="/">
+          <Tooltip title="Logout">
+            <IconButton sx={{
+                backgroundColor: "#476C92",
+                color: "white",
+                textTransform: "capitalize",
+                "&:hover": {
+                  backgroundColor: "white",
+                  color: "#476C92",
+                  borderColor: "#476C92",
+                  boxShadow: "none",
+                  border: "1px solid",
+                },
+              }} >
+              <LogoutIcon onClick={() => auth.signOut()}
+              
+              />
+            </IconButton>
+          </Tooltip>
+          </Link>
+        </Box>
+      </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+
+export default EditorHeader;
--- client/src/routes/Root.jsx
+++ client/src/routes/Root.jsx
@@ -1,15 +1,38 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../component/Footer";
 import Header from "../component/Header";
+import EditorHeader from "../component/editor/EditorHeader";
 
 function Root() {
+  const location = useLocation();
+  const isEditorPage = location.pathname.includes("/editor");
   return (
     <div>
-      <Header />
+      {isEditorPage ? <EditorHeader /> : <Header />}
       <Outlet />
-      <Footer />
+      {!isEditorPage && <Footer />}
     </div>
   );
 }
 
 export default Root;
+
+/*
+import { Outlet, useLocation } from "react-router-dom";
+import Footer from "../component/Footer";
+import Header from "../component/Header";
+import EditorHeader from "../component/editor/EditorHeader";
+
+function Root() {
+  const location = useLocation();
+  const isEditorPage = location.pathname.includes("/editor");
+  return (
+    <div>
+      {isEditorPage ? <EditorHeader /> : <Header />}
+      <Outlet />
+      {!isEditorPage && <Footer />}
+    </div>
+  );
+}
+
+export default Root;*/
