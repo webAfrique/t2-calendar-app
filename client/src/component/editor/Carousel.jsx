351b25ad0e9523c8b7fd4e1eb475b4f94054d557
Wen Yan <selinyan2016@gmail.com>
2024-05-08 09:48:06 +0300

    refine carousel

--- client/src/component/editor/Carousel.jsx
+++ client/src/component/editor/Carousel.jsx
@@ -30,11 +30,6 @@ const images = [
       "https://images.unsplash.com/photo-1576109026346-0e5cc7955858?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    label: "Santa",
-    imgPath:
-      "https://plus.unsplash.com/premium_photo-1661292179662-f67e85c9101a?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
     label: "Santa crystal ball",
     imgPath:
       "https://images.unsplash.com/photo-1551650322-a0cfff4dd76b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
